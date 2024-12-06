51038984879a9ecdc6137f220b0cc079546c0b5e
joshcoder2000 <jdd1993@hotmail.com>
2024-12-06 18:17:00 -0400

    Adds new section for certifications

---
+++ schemaTypes/certificationType.ts
@@ -0,0 +1,80 @@
+import {defineType, defineField} from 'sanity'
+
+export default defineType({
+  name: 'certification',
+  title: 'Certification',
+  type: 'document',
+  groups: [
+    {name: 'details', title: 'Details', default: true},
+    {name: 'assets', title: 'assets'},
+  ],
+  fieldsets: [{name: 'dates', title: 'Dates', options: {columns: 2}}],
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      group: 'details',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      group: 'details',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'certificationLink',
+      title: 'Certification Link',
+      type: 'url',
+      group: 'assets',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'startDate',
+      title: 'Start Date',
+      type: 'date',
+      group: 'details',
+      fieldset: 'dates',
+    }),
+    defineField({
+      name: 'endDate',
+      title: 'End Date',
+      type: 'date',
+      group: 'details',
+      fieldset: 'dates',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      group: 'assets',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'spanishImage',
+      title: 'Spanish Image',
+      type: 'image',
+      group: 'assets',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'englishFile',
+      title: 'English certification',
+      type: 'file',
+      group: 'assets',
+    }),
+    defineField({
+      name: 'spanishFile',
+      title: 'Spanish certification',
+      type: 'file',
+      group: 'assets',
+    }),
+  ],
+})
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import projects from './projectType'
 import profile from './profileType'
 import framework from './frameworkType'
+import certification from './certificationType'
 
-export const schemaTypes = [profile, projects, framework]
+export const schemaTypes = [profile, projects, framework, certification]
